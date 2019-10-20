9457c4a40d1812d9c701c7f71b62e8eda685a618
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-20 15:05:03 -0600

    Create footer element

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { 
   StyleSheet, 
   Text, 
   View, 
   Button,
-  ScrollView  
+  ScrollView,
+  TouchableOpacity 
 } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
 
-  
+  render() {
 
 
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Trex</Text>
+        <Text style={styles.header}>COOLNAMEHERE</Text>
         <ScrollView>
           <Text style={styles.text}>Welcome to Trex</Text>
-          <View style={styles.buttonsContainer}>
+          
             <Text style={styles.text}>I am a ...</Text>
-            <Button style={styles.button} title="Wanderer"/>
-            <Button style={styles.button} title="Follower" />
-          </View>
+            <TouchableOpacity>
+              <Text style={styles.button}>Wanderer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Follower</Text>
+            </TouchableOpacity>
+
+
         </ScrollView>
   
-      
+        <View style={styles.footer}>
+
+          <Text style={styles.footerText}>Trips</Text>
+          <Text style={styles.footerText}>Safety</Text>
+          <Text style={styles.footerText}>Followers</Text>
+        </View>
         
       </View>
     );
+
+    }
 }
 
 const styles = StyleSheet.create({
@@ -35,36 +48,49 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000',
     alignItems: 'stretch',
+    justifyContent: 'flex-start'
     
   }, 
   text: {
     color: 'white',
-    marginVertical: 90,
+    marginVertical: 40,
     textAlign: 'center',
-    fontSize: 30
+    fontSize: 30,
+    width: 'auto'
   },
   header: {
     backgroundColor: '#1C4263',
     color: 'white',
     paddingTop: 60,
     paddingLeft: 10,
-    // height: 'auto',
+    // height: 'auto',,
+    textAlign: 'center',
     top: 0,
     fontSize: 50,
   }, 
-  buttonsContainer: {
-    width: 100,
-    color: "#1C4263",
-    height: 30,
-    // flexDirection: "row",
-    borderWidth: 2,
-    borderColor: "#1C4263"
-  },
   button: {
     borderColor: 'white',
-    borderWidth: 4,
+    borderWidth: 1,
+    borderRadius: 8,
     borderStyle: 'solid',
-    width: 100,
+    width: 'auto',
+    height: 60,
+    margin: 20,
+    fontSize: 30,
+    padding: 10,
+    color: 'white',
+    textAlign: 'center',
     backgroundColor: '#1C4263'
+  },
+  footer: {
+    backgroundColor: '#1C4263',
+    flexDirection: 'row',
+    padding: 30,
+    justifyContent: 'space-around'
+  }, 
+  footerText: {
+    color: 'white',
+    marginBottom: 20,
+    fontSize: 20
   }
 });
