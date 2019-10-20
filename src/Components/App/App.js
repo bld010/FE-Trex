271713512394e0fde15e0f9a2b530d69aa780dce
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-20 14:20:53 -0600

    Setup initial app file structure
    Create footer element
    
    Add App test file
    
    Add touchable opacity to footer
    
    Add PR template
    
    Add PR Template

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,60 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { 
   StyleSheet, 
   Text, 
   View, 
   Button,
-  ScrollView  
+  ScrollView,
+  TouchableOpacity 
 } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
 
-  
+  render() {
 
 
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Trex</Text>
+        <Text style={styles.header}>Ariva</Text>
         <ScrollView>
-          <Text style={styles.text}>Welcome to Trex</Text>
-          <View>
+          <Text style={styles.text}>Welcome to Ariva</Text>
+          
             <Text style={styles.text}>I am a ...</Text>
-            <Button style={styles.button} title="Wanderer"/>
-            <Button title="Follower" />
-          </View>
+            <TouchableOpacity>
+              <Text style={styles.button}>Wanderer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Follower</Text>
+            </TouchableOpacity>
+
+
         </ScrollView>
   
-      
+        <View style={styles.footer}>
+          <TouchableOpacity>
+            <Text style={styles.footerText}>Trips</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.footerText}>Safety</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.footerText}>Followers</Text>
+          </TouchableOpacity>
+        </View>
         
       </View>
     );
+
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#000000',
     alignItems: 'stretch',
+    justifyContent: 'flex-start'
     
   }, 
   text: {
     color: 'white',
-    marginVertical: 90,
+    marginVertical: 40,
     textAlign: 'center',
-    fontSize: 30
+    fontSize: 30,
+    width: 'auto'
   },
   header: {
-    backgroundColor: 'purple',
+    backgroundColor: '#1C4263',
     color: 'white',
     paddingTop: 60,
     paddingLeft: 10,
-    // height: 'auto',
+    // height: 'auto',,
+    textAlign: 'center',
     top: 0,
     fontSize: 50,
   }, 
   button: {
     borderColor: 'white',
-    borderWidth: 4,
-    borderStyle: 'solid'
+    borderWidth: 1,
+    borderRadius: 8,
+    borderStyle: 'solid',
+    width: 'auto',
+    height: 60,
+    margin: 20,
+    fontSize: 30,
+    padding: 10,
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: '#1C4263'
+  },
+  footer: {
+    backgroundColor: '#1C4263',
+    flexDirection: 'row',
+    padding: 30,
+    justifyContent: 'space-around'
+  }, 
+  footerText: {
+    color: 'white',
+    marginBottom: 20,
+    fontSize: 20
   }
 });
