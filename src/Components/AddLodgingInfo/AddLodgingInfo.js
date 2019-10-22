a3dead2b068f1104dec9ca7de784dc9554ccfe2d
Emily <lalonde0902@gmail.com>
2019-10-22 16:02:42 -0600

    Add initial setup for addlodginginfo and addtransportinfo

---
+++ src/Components/AddLodgingInfo/AddLodgingInfo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    ScrollView,
+    TouchableOpacity 
+  } from 'react-native';
+
+export default class AddLodgingInfo extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Text>Hey</Text>
+    )
+  }
+}
---
+++ src/Components/AddTransportInfo/AddTransportInfo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    ScrollView,
+    TouchableOpacity 
+  } from 'react-native';
+
+export default class AddTransportInfo extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Text>Hey</Text>
+    )
+  }
+}
