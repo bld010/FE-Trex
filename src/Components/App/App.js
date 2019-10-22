d275e090abe819f84295122c844db5befa59a329
Emily <lalonde0902@gmail.com>
2019-10-22 15:44:08 -0600

    Fix merge conflict in App with extra imports

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,6 @@
-import React, { Component } from 'react';
-<<<<<<< HEAD
-import Dashboard from '../Dashboard/Dashboard'
-import LegForm from '../LegForm/LegForm'
-import Disclaimer from '../Disclaimer/Disclaimer'
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  Button,
-  ScrollView,
-  TouchableOpacity 
-} from 'react-native';
-=======
-
+import React, { Component } from 'react'
 import AppNavigator from '../AppNavigator';
->>>>>>> master
+
 
 export default class App extends Component {
 
