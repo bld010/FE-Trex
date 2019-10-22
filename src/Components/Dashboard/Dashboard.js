8029959b6a2e076255523d7006ae00372821a872
Emily <lalonde0902@gmail.com>
2019-10-22 13:21:49 -0600

    Add initial setup for dashboard component and dashboard test

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ScrollView,
+  TouchableOpacity 
+} from 'react-native';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <View>
+      <Text>Hey</Text>
+      </View>
+    )
+  }
+}
---
+++ src/Components/Dashboard/Dashboard.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { Dashboard } from './Dashboard';
+
+describe('Dashboard', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
