10f76d0218686ae0031cbdb3216af0d76d6b2f0d
Emily <lalonde0902@gmail.com>
2019-10-22 15:58:26 -0600

    Install jest to run dummy tests

---
+++ src/Components/AddLodgingInfo/AddLodgingInfo.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { AddLodgingInfo } from './AddLodgingInfo';
+
+describe('AddLodgingInfo', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
---
+++ src/Components/AddTransportInfo/AddTransportInfo.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { AddTransportInfo } from './AddTransportInfo';
+
+describe('AddTransportInfo', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
