0b76d9a8f49a4ca95ca0ccb4ee2579f6a2bc21bd
Emily <lalonde0902@gmail.com>
2019-10-22 13:22:56 -0600

    Add initial setup for legform component and legform test

---
+++ src/Components/LegForm/LegForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+
+export default class LegForm extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <View>
+      <Text>hello</Text>
+      </View>
+    )
+  }
+}
---
+++ src/Components/LegForm/LegForm.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { LegForm } from './LegForm';
+
+describe('LegForm', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
