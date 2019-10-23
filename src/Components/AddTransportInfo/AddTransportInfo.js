e1ac0c81b81eb00a18cdb5c638ce3939179e6a27
Emily <lalonde0902@gmail.com>
2019-10-23 10:57:38 -0600

    Add state and handleInputs method to addlodginginfo and addtransportinfo components

--- src/Components/AddTransportInfo/AddTransportInfo.js
+++ src/Components/AddTransportInfo/AddTransportInfo.js
@@ -1,22 +1,90 @@
-import React, { Component } from 'react';
-import { 
-    StyleSheet, 
-    Text, 
-    View, 
-    ScrollView,
-    TouchableOpacity 
-  } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 
 export default class AddTransportInfo extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-
-    }
+      startTrans: '',
+      endTrans: '',
+      dateTrans: ''
+    };
   }
+
+  handleInputs = () => {
+    this.setState({ [e.target.name] : e.target.value })
+  }
+
   render() {
     return (
-      <Text>Hey</Text>
-    )
+      <ScrollView>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Start Destination"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+            value={this.state.startTrans}
+            onChange={this.handleInputs}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="End Destination"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+            value={this.state.endTrans}
+            onChange={this.handleInputs}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Date"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+            value={this.state.dateTrans}
+            onChange={this.handleInputs}
+          />
+          <TouchableOpacity>
+            <Text style={styles.button}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 15
+  },
+  textInput: {
+    backgroundColor: "white",
+    borderColor: "#CCCCCC",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    fontSize: 25,
+    marginTop: 15,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  button: {
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 8,
+    borderStyle: "solid",
+    width: "auto",
+    height: 60,
+    margin: 20,
+    fontSize: 30,
+    padding: 10,
+    color: "white",
+    textAlign: "center",
+    backgroundColor: "#1C4263"
+  }
+});
