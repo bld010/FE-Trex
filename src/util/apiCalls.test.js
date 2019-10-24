bb39a66a6a967a75160170b170979015bfab7c02
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-23 18:52:27 -0600

    Setup dummy test for apiCalls

---
+++ src/util/apiCalls.test.js
@@ -0,0 +1,15 @@
+import {
+  fetchMyTrips
+} from './apiCalls';
+
+describe('apiCalls', () => {
+
+  describe('fetchMyTrips', () => {
+
+    it.only('should do a thing', () => {
+      expect(true).toEqual(true)
+      
+    })
+  })
+
+})
