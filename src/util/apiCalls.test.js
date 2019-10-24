a86626fcc0dfc75c194445e1dbd79a1c79e9a294
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-24 17:29:27 -0600

    Complete tests for postNewTrip

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -110,20 +110,103 @@ describe('apiCalls', () => {
   })
 
   describe('postNewTrip', () => {
-    it('should call fetch with proper url and query params', () => {
 
-    })
+    let mockFetch;
+    let queryParams;
+    let url;
+    let options;
+    let mockTripInfo;
 
-    it('should return a trip object when successful (HAPPY)', () => {
+    beforeEach(() => {
+      mockFetch = jest.fn()
+      global.fetch = mockFetch;
 
-    })
+      mockTripInfo = {
+        name: "Venezuela",
+        startDate: "2019-08-02",
+        endDate: "2018-08-22", 
+        userId: 4
+      }
 
-    it('should return an error if response status is not ok (SAD)', () => {
-
-    })
-
-    it('should return an error if the fetch fails (SAD)', () => {
+      let queryParams = `mutation {createTrip(input: {name: "Venezuela", startDate: "2019-08-02", endDate: "2018-08-22", userId: 4}) {trip {name startDate endDate id}}}`
       
+      url = `https://secret-cliffs-17751.herokuapp.com/graphql?query=${queryParams}`
+      
+      options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      } 
+    })
+
+    it('should call fetch with proper url and query params', async () => {
+
+      mockFetch.mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => { 
+            return (
+              { data: {
+                  createTrip: {
+                    trip: {}
+                  }
+                }
+              }
+            )}
+          })
+        })
+
+        await postNewTrip(mockTripInfo)
+
+        expect(mockFetch).toHaveBeenCalledWith(url, options)
+
+    })
+
+    it('should return a trip object when successful (HAPPY)', async () => {
+
+      mockFetch.mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => { 
+            return (
+              { data: {
+                  createTrip: {
+                    trip: {...mockTripInfo, id: 12}
+                  }
+                }
+              }
+            )}
+          })
+        })
+
+        let expected = {...mockTripInfo, id: 12}
+
+        await expect(postNewTrip(mockTripInfo)).resolves.toEqual(expected)
+
+    })
+
+    it('should return an error if response status is not ok (SAD)', async () => {
+
+      mockFetch.mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+          })
+        })
+
+      await expect(postNewTrip(mockTripInfo)).rejects.toEqual(Error('There was an error creating your trip'))
+
+    })
+
+    it('should return an error if the fetch fails (SAD)', async () => {
+
+      mockFetch.mockImplementation(() => {
+        return Promise.reject(Error('There was an error creating your trip'))
+      })
+
+      await expect(postNewTrip(mockTripInfo)).rejects.toEqual(Error('There was an error creating your trip'))
+
+
     })
   })
 
