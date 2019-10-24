1fa0801712b7d0bb9aced6a5a8bc58210e831d04
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-24 16:22:21 -0600

    Rename TripForm state property names to match required parameters for backend

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -25,3 +25,39 @@ export const fetchMyTrips = async (user_id) => {
   }
 }
 
+export const postNewTrip = async (user_id) => {
+
+  let trip = {
+    name,
+    startDate,
+    endDate,
+    userId: user_Id
+  }
+
+  let queryParams = `
+  mutation {
+    createTrip(input: {name: "Austraila!", userId: 1, startDate: "", endDate: "Dec 1st"}) {
+      trip {
+        name
+        startDate
+        endDate
+      }
+    }
+  }
+}`
+
+  let url = `https://secret-cliffs-17751.herokuapp.com/graphql?query=${queryParams}`
+
+  try {
+    let resp = await fetch(url,options)
+    if (!resp.ok) {
+      throw new Error('There was an error creating your trip')
+    }
+
+    let data = await resp.json();
+    let trip_id
+  } catch (error) {
+    throw error
+  }
+
+}
