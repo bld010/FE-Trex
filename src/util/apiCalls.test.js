e5d15a1960c634e8aa76d29ffaf6bf6a1774e460
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-24 13:38:52 -0600

    Finish tests for fetchMyTrips

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -7,34 +7,28 @@ describe('apiCalls', () => {
   describe('fetchMyTrips', () => {
 
     let mockFetch;
+    let queryParams;
+    let url;
+    let options;
 
     beforeEach(() => {
       mockFetch = jest.fn()
       global.fetch = mockFetch;
-    })
 
-    it('should call fetch with correct url and queryParams', async () => {
+      queryParams = `{user(id: 1) {trips {id, name, startDate, endDate}}}`
+  
+      url = `https://secret-cliffs-17751.herokuapp.com/graphql?query=${queryParams}`
       
-      let queryParams = `{
-       user(id: 1) {
-         trips {
-           id
-           name
-           startDate
-           endDate
-         }
-       }
-      }`
-
-      let url = `https://secret-cliffs-17751.herokuapp.com/graphql?query=${queryParams}`
-      
-      let options = {
+      options = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         }
-      }
+      } 
+    })
 
+    it('should call fetch with correct url and queryParams', async () => {
+      
       mockFetch.mockImplementation(() => {
         return Promise.resolve({
           ok: true,
@@ -46,23 +40,69 @@ describe('apiCalls', () => {
                 }
                 }
               }
-        )}
-      })
-    })
+            )}
+          })
+        })
+
         await fetchMyTrips(1)
 
         expect(mockFetch).toHaveBeenCalledWith(url, options)
     })
       
-    it('should return the trip (HAPPY)', () => {
+    it('should return the trips for a specific user (HAPPY)', async () => {
+      let mockTrips = [{
+        "endDate": "2020-01-02",
+        "id": "1",
+        "name": "Vanuatu",
+        "startDate": "2019-07-11",
+      },
+      {
+        "endDate": "2020-04-09",
+        "id": "2",
+        "name": "Lithuania",
+        "startDate": "2019-09-28",
+      }]
+
+      mockFetch.mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => { 
+            return (
+              { data: {
+                user: {
+                  trips: mockTrips
+                }
+                }
+              }
+            )}
+          })
+        })
+
+      let response = await fetchMyTrips(1)
+
+      expect(response).toEqual(mockTrips)
 
     })
 
-    it('should throw an error when fetch fails (SAD)', () => {
+    it('should throw an error when fetch fails (SAD)', async () => {
+
+      mockFetch.mockImplementation(() => {
+        return Promise.reject(Error('There was an error fetching your trips'))
+      })
+
+      await expect(fetchMyTrips(1)).rejects.toEqual(Error('There was an error fetching your trips'))
 
     })
 
-    it('should throw an error if response is not ok (SAD)', () => {
+    it('should throw an error if response is not ok (SAD)', async () => {
+      
+      mockFetch.mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+          })
+        })
+
+      await expect(fetchMyTrips(1)).rejects.toEqual(Error('There was an error fetching your trips'))
 
     })
   })
