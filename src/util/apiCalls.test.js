c247efa5cec7917749932df760d09ff137e962f8
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-23 19:19:30 -0600

    Setup it blocks for fetchMyTrips tests

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -6,8 +6,20 @@ describe('apiCalls', () => {
 
   describe('fetchMyTrips', () => {
 
-    it.only('should do a thing', () => {
-      expect(true).toEqual(true)
+    it('should call fetch with correct url and queryParams', () => {
+      
+      
+    })
+
+    it('should return the trip (HAPPY)', () => {
+
+    })
+
+    it('should throw an error when fetch fails (SAD)', () => {
+
+    })
+
+    it('should throw an error if response is not ok (SAD)', () => {
       
     })
   })
