e9733e62827408baed65bfe7bcbb220c6ca802a9
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-25 08:50:59 -0600

    Setup dummy tests in all files

---
+++ src/Components/Trip/Trip.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { Trip } from './Trip';
+
+describe('Trip', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
---
+++ src/Components/TripForm/TripForm.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { TripForm } from './TripForm';
+
+describe('TripForm', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
