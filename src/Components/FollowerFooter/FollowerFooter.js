f37010bb048c40a4a5bf4d52e4206939dfbdc98c
Emily <lalonde0902@gmail.com>
2019-10-26 12:48:36 -0600

    Add navigation to follower default messages to make sure it displays correctlt

--- src/Components/FollowerFooter/FollowerFooter.js
+++ src/Components/FollowerFooter/FollowerFooter.js
@@ -1,45 +1,39 @@
-import React, { Component } from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  TouchableOpacity
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default class Footer extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
   }
-  
+
   render() {
-
-
-    return(
+    return (
       <View style={styles.footer}>
-            <TouchableOpacity
-            
-            onPress={() => this.props.navigate('FollowerDashboard')}
-            >
-              <Text style={styles.footerText}>Home</Text>
-            </TouchableOpacity>
-
-          </View>
-    )
+        <TouchableOpacity
+          onPress={() => this.props.navigate("FollowerDashboard")}
+        >
+          <Text style={styles.footerText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.props.navigate("DefaultFollowerMessages")}
+        >
+          <Text style={styles.footerText}>Messages</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
 
-
 const styles = StyleSheet.create({
   footer: {
-    backgroundColor: '#84183B',
-    flexDirection: 'row',
+    backgroundColor: "#84183B",
+    flexDirection: "row",
     padding: 30,
-    justifyContent: 'space-around'
-  }, 
+    justifyContent: "space-around"
+  },
   footerText: {
-    color: 'white',
+    color: "white",
     marginBottom: 20,
     fontSize: 20
   }
-})
+});
