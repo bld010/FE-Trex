a994670a6d9d81d65b7ad8f7fc41092ed43e4c37
Emily <lalonde0902@gmail.com>
2019-10-26 13:29:23 -0600

    Add final dummy tests so that all components have test files set up

---
+++ src/Components/TravelCompanionsContacts/TravelCompanionsContacts.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { TravelCompanionsContacts } from './TravelCompanionsContacts';
+
+describe('TravelCompanionsContacts', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
