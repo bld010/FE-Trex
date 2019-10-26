8f2c62041c472fef8e03ae56956f23a9f0990c48
Emily <lalonde0902@gmail.com>
2019-10-26 13:29:23 -0600

    Add final dummy tests so that all components have test files set up

---
+++ src/Components/MyWanderersTrips/MyWanderersTrips.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { MyWanderersTrips } from './MyWanderersTrips';
+
+describe('MyWanderersTrips', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
