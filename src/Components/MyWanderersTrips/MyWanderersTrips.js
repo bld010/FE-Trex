d2e4affa9a55191e795858e3e62b99593b14b06c
Emily <lalonde0902@gmail.com>
2019-10-26 11:47:00 -0600

    Add basic component set up for followers dashboard, messages, and their wanderer trip view

---
+++ src/Components/MyWanderersTrips/MyWanderersTrips.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Keyboard
+} from "react-native";
+
+export default class MyWanderersTrips extends Component {
+  constructor() {
+    super() 
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Text>Wanderer trip info</Text>
+    )
+  }
+}
