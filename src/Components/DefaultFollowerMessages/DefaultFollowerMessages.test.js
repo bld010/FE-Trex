781c3a97bcd0b8f184851733acf593fd8ca1ba81
Emily <lalonde0902@gmail.com>
2019-10-26 13:23:55 -0600

    Add dummy test for default follower messages and app

---
+++ src/Components/DefaultFollowerMessages/DefaultFollowerMessages.test.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { DefaultFollowerMessages } from './DefaultFollowerMessages';
+
+describe('DefaultFollowerMessages', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
