1859dd4dafdb4b1d652eb44710b05913f507f776
Emily <lalonde0902@gmail.com>
2019-10-26 12:38:33 -0600

    Add header and footer styling and added the components to render inside of follower dashboard

--- src/Components/FollowerDashboard/FollowerDashboard.js
+++ src/Components/FollowerDashboard/FollowerDashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Home from '../Home/Home'
+import FollowerHeader from '../FollowerHeader/FollowerHeader'
+import FollowerFooter from '../FollowerFooter/FollowerFooter'
 import {
   StyleSheet,
   Text,
@@ -18,11 +19,45 @@ export default class FollowerDashboard extends Component {
     }
   }
   render() {
-    const {navigate} = this.props.navigation;
     return (
-      <View>
-        <Text>Display Wanderers that user follows</Text>
+      <View style={styles.container}>
+          <FollowerHeader />
+      <ScrollView>
+        <Text style={styles.text}>Display Wanderers that user follows</Text>
+      </ScrollView>
+        <FollowerFooter />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start'
+  }, 
+  text: {
+    color: 'white',
+    marginVertical: 40,
+    textAlign: 'center',
+    fontSize: 30,
+    width: 'auto'
+  }, 
+  button: {
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderStyle: 'solid',
+    width: 'auto',
+    height: 60,
+    margin: 20,
+    fontSize: 30,
+    padding: 10,
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: '#84183B',
+    alignItems: 'stretch'
+  }
+});
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -47,8 +47,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
-    // alignItems: 'stretch',
-    // justifyContent: 'flex-start'
   }, 
   text: {
     color: 'white',
