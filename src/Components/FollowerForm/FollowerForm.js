9f5603d2a27365749d223f1e2ca71c33c39dea41
Emily <lalonde0902@gmail.com>
2019-10-27 13:06:32 -0600

    Remove unecessary styles from follower form

--- src/Components/FollowerForm/FollowerForm.js
+++ src/Components/FollowerForm/FollowerForm.js
@@ -1,89 +1,88 @@
-import React, { Component } from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
   ScrollView,
   TextInput,
-  TouchableOpacity ,
+  TouchableOpacity,
   Keyboard
-} from 'react-native';
-import WandererFooter from '../WandererFooter/WandererFooter';
-import WandererHeader from '../WandererHeader/WandererHeader';
+} from "react-native";
+import WandererFooter from "../WandererFooter/WandererFooter";
+import WandererHeader from "../WandererHeader/WandererHeader";
 
 export default class FollowerForm extends Component {
   constructor(props) {
-    super()
+    super();
     this.state = {
-      name: '',
-      followerName: '',
-      followerEmail: '',
-      followerPhoneNum: '',
-      followerAddress: '',
-      emergencyContact: false,
-    }
+      name: "",
+      followerName: "",
+      followerEmail: "",
+      followerPhoneNum: "",
+      followerAddress: "",
+      emergencyContact: false
+    };
   }
-  
+
   render() {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-
-      <WandererHeader />
+        <WandererHeader />
 
         <ScrollView>
-
           <View>
             <Text style={styles.title}>Add A New Follower</Text>
           </View>
           <View style={styles.container}>
-          <Text style={styles.label}>Name</Text>
-          <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter Followers Name..."
-            placeholderTextColor='white'
-            maxLength={20}
-            onBlur={Keyboard.dismiss}
-            value={this.state.followerName}
-            onChangeText={followerName => this.setState({ followerName })}
-          />
+            <Text style={styles.label}>Name</Text>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Followers Name..."
+                placeholderTextColor="white"
+                maxLength={20}
+                onBlur={Keyboard.dismiss}
+                value={this.state.followerName}
+                onChangeText={followerName => this.setState({ followerName })}
+              />
+            </View>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Followers Email..."
+                placeholderTextColor="white"
+                maxLength={20}
+                onBlur={Keyboard.dismiss}
+                value={this.state.followerEmail}
+                onChangeText={followerEmail => this.setState({ followerEmail })}
+              />
+            </View>
+            <Text style={styles.label}>Phone Number</Text>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Followers Phone Number"
+                placeholderTextColor="white"
+                maxLength={20}
+                onBlur={Keyboard.dismiss}
+                value={this.state.followerPhoneNum}
+                onChangeText={followerPhoneNum =>
+                  this.setState({ followerPhoneNum })
+                }
+              />
+            </View>
+            <TouchableOpacity>
+              <Text style={styles.button}>Save</Text>
+            </TouchableOpacity>
           </View>
-          <Text style={styles.label}>Email</Text>
-          <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter Followers Email..."
-            placeholderTextColor='white'
-            maxLength={20}
-            onBlur={Keyboard.dismiss}
-            value={this.state.followerEmail}
-            onChangeText={followerEmail => this.setState({ followerEmail })}
-          />
-          </View>
-          <Text style={styles.label}>Phone Number</Text>
-          <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter Followers Phone Number"
-            placeholderTextColor='white'
-            maxLength={20}
-            onBlur={Keyboard.dismiss}
-            value={this.state.followerPhoneNum}
-            onChangeText={followerPhoneNum => this.setState({ followerPhoneNum })}
-          />
-          </View>
-          <TouchableOpacity>
-            <Text style={styles.button}>Save</Text>
-          </TouchableOpacity>
-          </View>
-        </ScrollView>        
+        </ScrollView>
         <WandererFooter navigate={navigate} />
       </View>
     );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -91,9 +90,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000000",
     alignItems: "stretch",
     justifyContent: "flex-start"
-  },
-  inputContainer: {
-    marginTop: 15
   },
   title: {
     textAlign: "center",
@@ -108,67 +104,24 @@ const styles = StyleSheet.create({
     paddingVertical: 12
   },
   input: {
-    backgroundColor: 'black',
-    color: 'white',
+    backgroundColor: "black",
+    color: "white",
     fontSize: 18,
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     marginLeft: 10
   },
   form: {
-    borderColor: 'white',
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 8,
     borderStyle: "solid",
     height: 60,
     width: 350,
-    color: 'white',
+    color: "white",
     padding: 10,
     marginLeft: 15,
     marginBottom: 20
-  },
-  sideBySideContainer: {
-    flex: 1,
-    backgroundColor: "#000000",
-    flexDirection: "row",
-    justifyContent: "space-around"
-    // justifyContent: 'flex-start'
-  },
-  buttonText: {
-    fontSize: 20,
-    color: "white",
-    textAlign: "center",
-    paddingVertical: 10
-  },
-  button: {
-    borderColor: "#768DA1",
-    borderWidth: 1,
-    borderRadius: 8,
-    borderStyle: "solid",
-    width: "auto",
-    height: 60,
-    margin: 20,
-    fontSize: 30,
-    padding: 10,
-    color: "white",
-    textAlign: "center",
-    backgroundColor: "#1C4263",
-    alignItems: "stretch"
-  },
-  sideBySideButton: {
-    width: 170,
-    borderColor: "#768DA1",
-    borderWidth: 1,
-    borderRadius: 8,
-    borderStyle: "solid",
-    height: 60,
-    margin: 20,
-    fontSize: 30,
-    padding: 10,
-    color: "white",
-    textAlign: "center",
-    backgroundColor: "#1C4263",
-    alignItems: "stretch"
   },
   button: {
     borderColor: "white",
