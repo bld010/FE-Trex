97e6951ec2d40f4a482ecc8a89ddde621e0781a0
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-29 17:23:10 -0600

    Add getParams mock to test files

--- src/Components/MyTrips/MyTrips.test.js
+++ src/Components/MyTrips/MyTrips.test.js
@@ -4,18 +4,19 @@ import { MyTrips } from './MyTrips';
 import { shallow } from 'enzyme';
 
 describe('MyTrips', () => {
+
   it('should return true', () => {
     expect(true).toEqual(true)
   })
 
   it('should match the snapshot when loading', () => {
-    let wrapper = shallow(<MyTrips navigation={{ navigate: jest.fn() }} />)
+    let wrapper = shallow(<MyTrips navigation={{ navigate: jest.fn(), getParam: jest.fn() }} />)
 
     expect(wrapper).toMatchSnapshot();
   })
 
   it('should match the snapshot when trips array is populated', () => {
-    let wrapper = shallow(<MyTrips navigation={{ navigate: jest.fn() }} />)
+    let wrapper = shallow(<MyTrips navigation={{ navigate: jest.fn(), getParam: jest.fn() }} />)
 
     wrapper.instance().setState({ trips: [{id: 1, name: 'Paris'}, {id: 2, name: 'Rome'}]})
 
