894b50f237459c8ceb1433238f52270ced8d17a4
Emily <lalonde0902@gmail.com>
2019-10-30 11:19:42 -0600

    Add handle save to add lodging info component

--- src/Components/AddLodgingInfo/AddLodgingInfo.js
+++ src/Components/AddLodgingInfo/AddLodgingInfo.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import DatePicker from "react-native-datepicker";
-import MapInputFirst from '../MapInput/MapInputFirst'
-import WandererHeader from '../WandererHeader/WandererHeader';
-import WandererFooter from '../WandererFooter/WandererFooter';
-import { postNewLodging, patchLodging, deleteLodging } from '../../util/apiCalls';
+import MapInputFirst from "../MapInput/MapInputFirst";
+import WandererHeader from "../WandererHeader/WandererHeader";
+import WandererFooter from "../WandererFooter/WandererFooter";
+import {
+  postNewLodging,
+  patchLodging,
+  deleteLodging
+} from "../../util/apiCalls";
 import {
   StyleSheet,
   Text,
@@ -12,33 +16,33 @@ import {
   TouchableOpacity,
   Keyboard,
   TextInput
-} from 'react-native';
+} from "react-native";
 
 export default class AddLodgingInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: '',
-      name: '',
-      arrivalDate: '',
-      departureDate: '',
-      lodging: this.props.navigation.getParam('lodging') || null,
-      legId: this.props.navigation.getParam('legId'),
-      userId: this.props.navigation.getParam('userId')
+      city: "",
+      name: "",
+      arrivalDate: "",
+      departureDate: "",
+      lodging: this.props.navigation.getParam("lodging") || null,
+      legId: this.props.navigation.getParam("legId"),
+      userId: this.props.navigation.getParam("userId")
     };
   }
 
   componentDidMount = () => {
     if (this.state.lodging) {
-      let { name, arrivalDate, departureDate, city } = this.state.leg
+      let { name, arrivalDate, departureDate, city } = this.state.leg;
       this.setState({
         name,
         arrivalDate,
         departureDate,
         city
-      })
+      });
     }
-  }
+  };
 
   handlerFirstInput(arg) {
     this.setState({
@@ -46,85 +50,85 @@ export default class AddLodgingInfo extends Component {
     });
     return;
   }
-  
+
   checkLodgingParams = () => {
-    let { 
-      city,
-      name,
-      arrivalDate,
-      departureDate
-    } = this.state;
+    let { city, name, arrivalDate, departureDate } = this.state;
 
     if (
-      city === '' ||
-      name === '' ||
-      arrivalDate === '' ||
-      departureDate === ''
+      city === "" ||
+      name === "" ||
+      arrivalDate === "" ||
+      departureDate === ""
     ) {
-      this.setState({ error: 'Please fill out all fields'})
+      this.setState({ error: "Please fill out all fields" });
       return false;
     } else {
-      this.setState({ error: ''});
+      this.setState({ error: "" });
       return true;
     }
-  }
+  };
 
   handleSave = async () => {
-    let formIsFilledCorrectly = this.checkLodgingParams();
-    if (formIsFilledCorrectly) {
-      //fire post call here
-    }
+    let updatedLegId;
 
-  }
+    if (!this.props.navigation.getParam("lodging")) {
+      let formIsFilledCorrectly = this.checkLodgingParams();
+      if (formIsFilledCorrectly) {
+        updatedLegId = await this.createNewLodging();
+        this.props.navigation.navigate("Leg", { legId: updatedLegId });
+      }
+    } else {
+      updatedLegId = await this.editLodging();
+      this.props.navigation.navigate('Leg', {legId: updatedLegId})
+    }
+  };
 
   createNewLodging = async () => {
-    let {name, city, arrivalDate, departureDate, legId} = this.state;
+    let { name, city, arrivalDate, departureDate, legId } = this.state;
     let newLodgingInfo = {
       name,
       city,
       arrivalDate,
       departureDate,
       legId
-    }
+    };
 
     try {
       let updatedLegId = await postNewLodging(newLodgingInfo);
-      return updatedLegId
+      return updatedLegId;
+    } catch (error) {
+      this.setState({ error: "There was an error creating your lodging" });
     }
-    catch (error) {
-      this.setState({ error: 'There was an error creating your lodging'})
-    }
-  }
+  };
 
   editLodging = async () => {
-    let id = this.state.lodging.id
-    let {name, city, arrivalDate, departureDate, legId} = this.state;
+    let id = this.state.lodging.id;
+    let { name, city, arrivalDate, departureDate, legId } = this.state;
 
-    let editedLodgingInfo = { 
+    let editedLodgingInfo = {
       name,
       city,
       arrivalDate,
       departureDate,
       legId,
       id
-    }
+    };
     try {
-      let editedLegId = await patchLodging(editedLodgingInfo)
-      return editedLegId
+      let editedLegId = await patchLodging(editedLodgingInfo);
+      return editedLegId;
+    } catch (error) {
+      this.setState({ error: "There was an error editing your lodging" });
     }
-    catch (error) {
-      this.setState({error: 'There was an error editing your lodging'})
-    }
-  }
+  };
 
   removeLodging = async () => {
     try {
       await deleteLodging(this.state.lodging.id);
-      this.props.navigation.navigate('Leg')
+      this.props.navigation.navigate("Leg");
     } catch (error) {
-      this.setState({ error: 'There was an error deleting your lodging'})
+      this.setState({ error: "There was an error deleting your lodging" });
     }
-  }
+  };
 
   render() {
     const { navigate } = this.props.navigation;
@@ -137,13 +141,15 @@ export default class AddLodgingInfo extends Component {
               <Text style={styles.title}>Add Lodging</Text>
             </View>
             <Text style={styles.labelCity}>City</Text>
-            <MapInputFirst handlerFirstInput={this.handlerFirstInput.bind(this)} />
+            <MapInputFirst
+              handlerFirstInput={this.handlerFirstInput.bind(this)}
+            />
             <Text style={styles.label}>Name</Text>
             <View style={styles.form}>
               <TextInput
                 style={styles.input}
-                placeholder='Enter Lodging Name...'
-                placeholderTextColor='black'
+                placeholder="Enter Lodging Name..."
+                placeholderTextColor="black"
                 maxLength={20}
                 onBlur={Keyboard.dismiss}
                 value={this.state.name}
@@ -154,12 +160,12 @@ export default class AddLodgingInfo extends Component {
             <DatePicker
               style={{ width: 370, height: 65 }}
               date={this.state.arrivalDate}
-              mode='date'
-              placeholder='Start Date'
-              placeholderTextColor='white'
-              format='MM-DD-YYYY'
-              confirmBtnText='Confirm'
-              cancelBtnText='Cancel'
+              mode="date"
+              placeholder="Start Date"
+              placeholderTextColor="white"
+              format="MM-DD-YYYY"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
               customStyles={{
                 dateIcon: {
                   left: 0,
@@ -168,15 +174,15 @@ export default class AddLodgingInfo extends Component {
                 dateInput: {
                   marginLeft: 15,
                   color: "black",
-                  backgroundColor: 'white',
+                  backgroundColor: "white",
                   height: 60,
                   borderRadius: 8,
                   borderWidth: 1,
-                  borderColor: "white",
+                  borderColor: "white"
                 },
                 dateText: {
                   fontSize: 22,
-                  color: "black",
+                  color: "black"
                 },
                 placeholderText: {
                   fontSize: 22,
@@ -191,13 +197,13 @@ export default class AddLodgingInfo extends Component {
             <DatePicker
               style={{ width: 370, height: 65 }}
               date={this.state.departureDate}
-              mode='date'
-              mode='date'
-              placeholder='Start Date'
-              placeholderTextColor='white'
-              format='MM-DD-YYYY'
-              confirmBtnText='Confirm'
-              cancelBtnText='Cancel'
+              mode="date"
+              mode="date"
+              placeholder="Start Date"
+              placeholderTextColor="white"
+              format="MM-DD-YYYY"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
               customStyles={{
                 dateIcon: {
                   left: 0,
@@ -206,15 +212,15 @@ export default class AddLodgingInfo extends Component {
                 dateInput: {
                   marginLeft: 15,
                   color: "black",
-                  backgroundColor: 'white',
+                  backgroundColor: "white",
                   height: 60,
                   borderRadius: 8,
                   borderWidth: 1,
-                  borderColor: "white",
+                  borderColor: "white"
                 },
                 dateText: {
                   fontSize: 22,
-                  color: "black",
+                  color: "black"
                 },
                 placeholderText: {
                   fontSize: 22,
@@ -225,10 +231,10 @@ export default class AddLodgingInfo extends Component {
                 this.setState({ departureDate: date });
               }}
             />
-            
-            {this.state.error !== '' && 
+
+            {this.state.error !== "" && (
               <Text style={styles.error}>{this.state.error}</Text>
-            }
+            )}
 
             <TouchableOpacity onPress={this.handleSave}>
               <Text style={styles.button}>Save</Text>
@@ -244,78 +250,78 @@ export default class AddLodgingInfo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'stretch',
-    justifyContent: 'flex-start'
+    backgroundColor: "#000000",
+    alignItems: "stretch",
+    justifyContent: "flex-start"
   },
   inputContainer: {
     marginTop: 15
   },
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 30,
-    color: 'white',
+    color: "white",
     paddingVertical: 25
   },
   text: {
     marginLeft: 20,
     fontSize: 20,
-    color: 'white',
+    color: "white",
     paddingVertical: 15
   },
   input: {
-    backgroundColor: 'white',
-    color: 'black',
+    backgroundColor: "white",
+    color: "black",
     fontSize: 18,
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     marginLeft: 10
   },
   form: {
-    backgroundColor: 'white',
-    borderColor: 'white',
+    backgroundColor: "white",
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 8,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     height: 60,
     width: 350,
-    color: 'white',
+    color: "white",
     padding: 10,
     marginLeft: 15,
     marginBottom: 20
   },
   button: {
-    borderColor: 'white',
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 8,
-    borderStyle: 'solid',
-    width: 'auto',
+    borderStyle: "solid",
+    width: "auto",
     height: 60,
     margin: 20,
     fontSize: 30,
     padding: 10,
-    color: 'white',
-    textAlign: 'center',
-    backgroundColor: '#1C4263'
+    color: "white",
+    textAlign: "center",
+    backgroundColor: "#1C4263"
   },
   label: {
     marginLeft: 20,
     fontSize: 20,
-    color: 'white',
+    color: "white",
     marginBottom: 5
   },
   labelCity: {
     marginLeft: 20,
     fontSize: 20,
-    color: 'white',
+    color: "white",
     marginBottom: -22,
     marginVertical: -10,
     color: "white"
   },
   error: {
-    color: 'white',
+    color: "white",
     fontSize: 25,
-    textAlign: 'center',
+    textAlign: "center",
     marginVertical: 15
   }
 });
