f721a5e207aa5525ea9993bc08ef840e9903fcd4
Emily <lalonde0902@gmail.com>
2019-10-30 14:46:13 -0600

    Set up lodging component

---
+++ src/Components/Lodging/Lodging.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import WandererHeader from "../WandererHeader/WandererHeader";
+import WandererFooter from "../WandererFooter/WandererFooter";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Keyboard,
+  TextInput
+} from "react-native";
+
+export default class Lodging extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Text>Hey</Text>
+    )
+  }
+}
